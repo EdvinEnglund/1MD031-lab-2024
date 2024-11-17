8ebaf7f7e87214df280cdf81caa85943e693a511
admn <admn@admns-MacBook-Pro.local>
2024-11-17 20:07:43 +0100

    upUntilComponentEventsJSVUE

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    {{ burger.name }} {{ burger.kCal }}
-  </div>
+  <div class="menuItem">
+                <h3>The {{ burger.name }} burger</h3>
+                <img v-bind:src="burger.image" class="pic">
+
+                <ul class="ingredients">
+                    <li>{{ burger.kCal }} kCal</li>
+                    <li class="alergens" v-if="burger.gluten">Gluten</li>
+                    <li class="alergens" v-if="burger.lactose">Lactose</li>
+                </ul>
+    </div>
 </template>
 
 <script>
@@ -15,5 +22,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Cormorant:wght@700&display=swap');
+/* Your comment goes here */
+.pic{
+  width: 440px;
+  height: 440px;
+}
 
+.menuItem {
+    background-color: #000;
+    color: #fff;
+    border-radius: 5px;
+    padding: 20px;
+    font-size: 100%;
+    border-radius: 50px;
+}
+.menuItem:hover {
+    color:#ff7000;
+ }
+ .alergens {
+    font-weight: bold;
+ }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,75 @@
 <template>
-<div>
-    <div>
-    <h1>Burgers</h1>
-    <Burger v-for="burger in burgers"
+
+<header>
+       
+       <img src="/img/many.png" id="top">
+        <h1 id="hello">Prince of burgers</h1> <!-- this is how u comment code--> 
+   </header>
+   
+   <section id="burger">
+      <h2>Select the burger of your choice</h2> 
+      <p>All of our burgers are made on order from 100% grass fed beef (or wild caught fish)</p>
+    <section class="wrapper">
+      <Burger v-for="burger in burgers"
             v-bind:burger="burger" 
             v-bind:key="burger.name"/>
-    </div>
-    <div id="map" v-on:click="addOrder">
-    click here
-    </div>
-</div>
+    </section>       
+
+   
+   </section>
+   <section id="delivery">
+       <h2>Delivery details</h2>
+       <p>As of now, we only deliver to adresses in Uppsala, Sweden</p>
+       <form>
+           <p>
+               <label for="fullname">Full Name</label><br>
+               <input v-model="fullname" id="fullname" required="required" placeholder="First and last name">
+           </p>
+           <p>
+               <label for="email">E-mail</label><br>
+               <input v-model="email" id="email" required="required" placeholder="E-mail address">
+           </p>
+           <p>
+               <label for="street">Street</label><br>
+               <input v-model="street" id="street" required="required" placeholder="Street name">
+           </p>
+           <p>
+               <label for="houseno">House</label><br>
+               <input v-model="houseno" id="houseno" required="required" placeholder="House number">
+           </p>
+
+           <p>
+               <label for="method">Payment method</label><br>
+               <select id="method" v-model="paymentMethod">
+                   <option>Credit/Debit Card</option>
+                   <option>Swish</option>
+                   <option>Cash (+10% administrative fee)</option>
+               </select>
+            </p>
+            <p>Your gender</p>
+ 
+            <input type="radio" id="man" name="gender" value="man" v-model="picked">
+            <label for="man">Man</label><br>
+            
+            <input type="radio" id="woman" name="gender" value="woman" v-model="picked">
+            <label for="woman">Woman</label><br>
+            
+            <input type="radio" id="Other" name="gender" value="other" v-model="picked">
+            <label for="other">Other</label><br>
+            
+            <input type="radio" id="none" name="gender" value="none" v-model="picked">
+            <label for="none">None of your business</label><br>
+       </form>
+       <button type="submit" v-on:click="printOrderData">
+           <img src="/img/shop.png" style="width:20px">
+           Place order
+         </button>
+   </section>
+   <hr>
+   <footer>
+       &copy; 2024 Prince Desiderius Burgers AB
+   </footer>
+
 </template>
 
 <script>
@@ -18,6 +78,26 @@ import io from 'socket.io-client'
 
 const socket = io("localhost:3000");
 
+class MenuItem {
+  constructor(name, kCal, gluten, lactose, image){
+    this.name = name;
+    this.kCal = kCal;
+    this.gluten = gluten;
+    this.lactose = lactose;
+    this.image = image;
+  }
+}
+/*
+var menu = [
+  new MenuItem("massive", 800, true, true, "/img/massive.jpg"),
+  new MenuItem("fishy", 350, true, false, "https://girlheartfood.com/wp-content/uploads/2020/06/Crispy-Fish-Burger-Recipe-9.jpg"),
+  new MenuItem("plain old", 450, true, true, "/img/plain.jpg")
+]
+*/
+
+import menu from '../assets/menu.json'
+console.log(menu);
+
 export default {
   name: 'HomeView',
   components: {
@@ -25,13 +105,18 @@ export default {
   },
   data: function () {
     return {
-      burgers: [ {name: "small burger", kCal: 250},
-                 {name: "standard burger", kCal: 450},
-                 {name: "large burger", kCal: 850}
-               ]
+      burgers: menu
     }
   },
   methods: {
+    printOrderData() {
+      console.log("Full Name:", this.fullname);
+      console.log("Email:", this.email);
+      console.log("Street:", this.street);
+      console.log("House:", this.houseno);
+      console.log("Payment Method:", this.paymentMethod);
+      console.log("Gender:", this.picked);
+    },
     getOrderNumber: function () {
       return Math.floor(Math.random()*100000);
     },
@@ -50,9 +135,83 @@ export default {
 </script>
 
 <style>
-  #map {
-    width: 300px;
-    height: 300px;
-    background-color: red;
-  }
+
+  @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Cormorant:wght@700&display=swap');
+/* Your comment goes here */
+.wrapper {
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: 33% 33% 33%;
+    background-color:  rgb(255, 199, 121);
+    color: #444;
+    border-radius: 50px;
+}
+
+.box {
+    background-color: #000;
+    color: #fff;
+    border-radius: 5px;
+    padding: 20px;
+    font-size: 100%;
+    border-radius: 50px;
+}
+.box:hover {
+    color:#ff7000;
+ }
+
+section {
+    margin:50px
+}
+#top{
+    opacity: 0.5;
+    width: 100%;
+    height: auto;
+}
+#hello{
+    position: absolute;
+    margin-top: -950px;
+    margin-left: 20px;
+    color:#fff
+}
+header {
+    margin:0px 75px;
+    overflow:hidden;
+    height:200px;
+    font-size: 36pt;
+}
+button {
+    margin:50px 0px
+}
+body {
+    font-size: 12pt;
+    font-family: Arial;
+}
+
+ .ingredients {
+    color: #ff7000;
+ }
+ 
+ #burger {
+    color: #000;
+    background-color: rgb(255, 199, 121);
+    border-style: dotted;
+    border-color: #000;
+    border-radius: 50px;
+ }
+
+ #burger h2, #burger p{
+    margin:20px;
+ }
+#delivery{
+    padding: 20px;
+    border-color: black;
+    border-style:dotted;
+    border-radius: 50px;
+
+}
+ button:hover {
+    background-color: #ff7000;
+    color: white
+ }
+
 </style>
